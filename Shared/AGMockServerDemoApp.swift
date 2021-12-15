7037add754f90c36393d893bfb67d68b81a0b417
Alexey Golovenkov <mobidevelop@gmail.com>
2021-12-16 00:41:49 +0400

    Initial commit

---
+++ Shared/AGMockServerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AGMockServerDemoApp.swift
+//  Shared
+//
+//  Created by Alex Golovenkov on 16.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct AGMockServerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
