369d177a701a228811f5095ee372c00e35ae33ad
Alexey Golovenkov <mobidevelop@gmail.com>
2021-12-18 20:38:44 +0400

    Remove UI tests

---
+++ AGMockServerDemoiOSTests/AGMockServerDemoiOSTests.swift
@@ -0,0 +1,38 @@
+//
+//  AGMockServerDemoiOSTests.swift
+//  AGMockServerDemoiOSTests
+//
+//  Created by Alex Golovenkov on 18.12.2021.
+//
+
+import XCTest
+import AGMockServer
+@testable import AGMockServerDemo
+
+class AGMockServerDemoiOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        AGMockServer.shared.unregisterAllHandlers()
+    }
+
+    func testNotHackedAddress() async throws {
+        let feed = try await NetworkDataProvider.shared.feed()
+        XCTAssertTrue(feed.title == "JSON Feed")
+        XCTAssertTrue(feed.home_page_url == "https://www.jsonfeed.org/")
+        XCTAssertTrue(feed.items?.count == 2)
+    }
+    
+    func testHackedRespose() async throws {
+        AGMockServer.shared.registerHandler(FeedHandler())
+        let feed = try await NetworkDataProvider.shared.feed()
+        XCTAssertTrue(feed.title == "Hacked Feed")
+        XCTAssertTrue(feed.home_page_url == "https://localhost/")
+        XCTAssertTrue(feed.items?.count == 1)
+    }
+
+}
